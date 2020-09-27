eae320c42c9e12cbb3ede8fb1fd741fe488f6856
Arjana <arjana99patel@zohomail.in>
2020-09-27 19:49:32 +0530

    Input- RNE
    Login page textinput changed

--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import {
- 
   View,
-  TextInput,
   ScrollView,
-  Alert
+  Alert,
   
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { ThemeProvider, Button, Text, } from 'react-native-elements';
+import { ThemeProvider, Button, Text, Input, } from 'react-native-elements';
 import theme from '../template/theme';
 import styles from '../template/styles';
 
@@ -30,61 +28,69 @@ export default class Login extends Component {
       
 
       <View style={styles.labelContainer}>
-          {/* <Label text="Username or Email" /> */}
-          <Text style={styles.textLabel}>Username or Email</Text>
-          <TextInput
-           style={styles.textInput}
-          />
+         
+            <Input
+                    placeholder='Username or Email'
+                    errorStyle={{ color: 'red' }}
+                    errorMessage='ENTER A VALID ERROR HERE'
+                    leftIcon={
+                        <Icon
+                        name='user'
+                        size={24}
+                        color='blue'
+                    />
+                    
+                    }
+                />
       
       </View> 
       <View style={styles.labelContainer}>
-
-      {/* <Label text="Password" /> */}
-          <Text style={styles.textLabel}>Password</Text>
-          <TextInput
-             secureTextEntry={true}
-              style={styles.textInput}
-          />
+            <Input 
+                placeholder="Password"
+                secureTextEntry={true} 
+                leftIcon={
+                    <Icon
+                    name='key'
+                    size={24}
+                    color='blue'
+                    />
+                }
+            
+            />
       </View>
 
       
 
       <View style={styles.labelContainer}>
-            {/* <Button 
-                styles={{button: styles.transparentButton}}
-                title=""
-            > */}
+           
                 <View style={styles.inline}>
                     <Icon name="facebook-official" size={30} color="#3B5699" />
                     <Text h2 h2Style = {styles.h2}> Connect</Text>
                     <Text h3 h3Style = {styles.h3}> with  Facebook</Text>
-                    {/* <Text style={[styles.lueText, styles.buttonBigText]}> Connect with  Facebook </Text>  */}
-                     {/* <Text style={styles.buttonBlueText}> with  Facebook </Text> */}
-                    
+                   
                 </View>
-            {/* </Button> */}
+           
         </View>
 
 
         <View style={styles.footer}>
-          <View style={styles.labelContainer}>
-          <ThemeProvider theme={theme.primaryButton}>
-              <Button 
-                  title="Sign In"
-                  //styles={{button: styles.primaryButton, textLabel: styles.buttonWhiteText}} 
-                  onPress={() => Alert.alert('Simple Button pressed')}
-              />
-          </ThemeProvider>
-          </View>
-          <View style={styles.labelContainer}>
-          <ThemeProvider theme={theme.defaultButton}>
-              <Button 
-                  title="CANCEL"
-                  //styles={{textLabel: styles.buttonBlackText}} 
-                  onPress={() => Alert.alert('Simple Button pressed')}
-              />
-          </ThemeProvider>
-          </View>
+            <View style={styles.labelContainer}>
+                <ThemeProvider theme={theme.primaryButton}>
+                    <Button 
+                        title="Sign In"
+                        onPress={() => Alert.alert('Simple Button pressed')}
+                    />
+                </ThemeProvider>
+            </View>
+            <View style={styles.labelContainer}>
+                <ThemeProvider theme={theme.defaultButton}>
+                    <Button 
+                        title="CANCEL"
+                        //styles={{textLabel: styles.buttonBlackText}} 
+                        onPress={() => Alert.alert('Simple Button pressed')}
+                    />
+                </ThemeProvider>
+            </View>
       </View>
 
       </ScrollView>
